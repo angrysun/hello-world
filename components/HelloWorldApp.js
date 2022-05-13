6e53a74490534505dd6d79b7b35e7e8c8d9d1406
Steven Vasquez <s.vasquez85@gmail.com>
2022-05-13 12:19:17 +0900

    TextInput component added

--- components/HelloWorldApp.js
+++ components/HelloWorldApp.js
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
-import { Button, Image, Text, TouchableOpacity, View, ScrollView } from 'react-native';
+import { Button, Image, Text, TextInput, TouchableOpacity, View, ScrollView } from 'react-native';
 import sun from './assets/sun.png';
 
 const HelloWorldApp = () => {
   const [pressedCount, setPressedCount] = useState(0);
+  const [name, setName] = useState('');
 
   return (
     <View
@@ -26,6 +27,17 @@ const HelloWorldApp = () => {
           : "The button isn't pressed yet"}
       </Text>
       <Button title="Press me" onPress={() => setPressedCount(pressedCount + 1)} disabled={pressedCount >= 3 ? true : false} />
+      <View style={{
+        padding: 16,
+      }}>
+        <Text style={{ marginVertical: 16 }}>
+          {name ? `Hi ${name}!` : 'What is your name?'}
+        </Text>
+        <TextInput
+          style={{ padding: 8, backgroundColor: '#f5f5f5' }}
+          onChangeText={setName}
+        />
+      </View>
       <View style={{ height: 400, backgroundColor: '#e5e5e5' }}>
         <ScrollView horizontal>
           <Image style={{ width: 100, height: 100 }} source={{ uri: 'https://picsum.photos/100/100' }}></Image>
