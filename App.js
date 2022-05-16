84b373161a6da4391d207fd56c4d3635f9035d65
Steven Vasquez <s.vasquez85@gmail.com>
2022-05-16 15:26:32 +0900

    fomik added to help with forms

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View, ScrollView } from 'react-native';
 import HelloWorldApp from './components/HelloWorldApp';
 import { Box } from './components/Box';
 import { Input } from './components/Input';
-import Constants from 'expo-constants';
+import { Formik } from 'formik'
 
 export default function App() {
   const [inputEmail, setInputEmail] = useState('');
@@ -15,56 +15,61 @@ export default function App() {
   const [inputToy, setInputToy] = useState('');
 
   return (
-    <ScrollView contentContainerStyle={styles.scroll} >
-      {/* <HelloWorldApp />
-      <Box color="blue" />
-      <StatusBar style="auto" /> */}
-      <Input
-        label="Email"
-        val={inputEmail}
-        setVal={setInputEmail}
-        holder={'Input your email here'}
-      />
-      <Input
-        label="Password"
-        val={password}
-        setVal={setPassword}
-        holder={'Input your password here'}
-        secureTextEntry
-      />
-      <Input
-        label="Confirm password"
-        holder={'Re-input your password here'}
-        onSubmitEditing={(e) => {
-          confirmPasswordsMatch(e.nativeEvent.text, password);
-        }}
-        secureTextEntry
-      />
-      <Input
-        label="Pet's name"
-        val={inputPetName}
-        setVal={setInputPetName}
-        holder={"Input your pet's name here"}
-      />
-      <Input
-        label="Pet's date of birth"
-        val={inputPetBday}
-        setVal={setInputPetBday}
-        holder={"Input your pet's birthday here"}
-      />
-      <Input
-        label="Breed"
-        val={inputBreed}
-        setVal={setInputBreed}
-        holder={"Input your pet's breed here"}
-      />
-      <Input
-        label="Favorite toy"
-        val={inputToy}
-        setVal={setInputToy}
-        holder={"Input your pet's favorite toy here"}
-      />
-    </ScrollView>
+    <Formik
+      initialValues={{ email: '', password: '', name: '', birthday: '', breed: '', toy: '' }}
+      onSubmit={values => console.log(values)}
+    >
+      <ScrollView contentContainerStyle={styles.scroll} >
+        {/* <HelloWorldApp />
+        <Box color="blue" />
+        <StatusBar style="auto" /> */}
+        <Input
+          label="Email"
+          val={inputEmail}
+          setVal={setInputEmail}
+          holder={'Input your email here'}
+        />
+        <Input
+          label="Password"
+          val={password}
+          setVal={setPassword}
+          holder={'Input your password here'}
+          secureTextEntry
+        />
+        <Input
+          label="Confirm password"
+          holder={'Re-input your password here'}
+          onSubmitEditing={(e) => {
+            confirmPasswordsMatch(e.nativeEvent.text, password);
+          }}
+          secureTextEntry
+        />
+        <Input
+          label="Pet's name"
+          val={inputPetName}
+          setVal={setInputPetName}
+          holder={"Input your pet's name here"}
+        />
+        <Input
+          label="Pet's date of birth"
+          val={inputPetBday}
+          setVal={setInputPetBday}
+          holder={"Input your pet's birthday here"}
+        />
+        <Input
+          label="Breed"
+          val={inputBreed}
+          setVal={setInputBreed}
+          holder={"Input your pet's breed here"}
+        />
+        <Input
+          label="Favorite toy"
+          val={inputToy}
+          setVal={setInputToy}
+          holder={"Input your pet's favorite toy here"}
+        />
+      </ScrollView>
+    </Formik>
   );
 }
 
@@ -84,7 +89,7 @@ const styles = StyleSheet.create({
   scroll: {
     flex: 1,
     justifyContent: 'center',
-    padding: Constants.statusBarHeight,
+    // padding: Constants.statusBarHeight,
     backgroundColor: '#ecf0f1',
   }
 });
