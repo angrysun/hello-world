bee69810e6fea90851b5a701fc5b4c6798cffa55
Steven Vasquez <s.vasquez85@gmail.com>
2022-05-16 18:41:07 +0900

    row flexDirection added

--- App.js
+++ App.js
@@ -123,6 +123,7 @@ function confirmPasswordsMatch(confirmationPassword, originalPassword) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    // flexDirection: 'row',
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
