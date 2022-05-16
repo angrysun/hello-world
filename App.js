ee83ea933fad636e3f1d0d04ba6b3e5587d3ea43
Steven Vasquez <s.vasquez85@gmail.com>
2022-05-16 10:35:41 +0900

    Box added with props

--- App.js
+++ App.js
@@ -2,11 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import HelloWorldApp from './components/HelloWorldApp';
+import { Box } from './components/Box';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <HelloWorldApp />
+      <Box color="blue" />
       <StatusBar style="auto" />
     </View>
   );
